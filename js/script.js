b53cbcaa3888eedbaeca640e537fff3e2e289858
Florvin Bayaborda <61019061+Florvin-04@users.noreply.github.com>
2023-03-09 12:11:03 +0800

    done challenge (frontendmentor.io)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 const burgerMenuBtn = document.querySelector("[hamburger-menu-button]");
 const mobileNavContainer = document.querySelector("[header-navigation]");
+const overlayBG = document.querySelector(".overlayBG");
+const body = document.querySelector("body");
+
+const emailInput = document.querySelector("#email");
+const invalidEmailLabel = document.querySelector(".validation");
 
 burgerMenuBtn.addEventListener("click", () => {
   mobileNavContainer.classList.toggle("active");
   burgerMenuBtn.classList.toggle("active");
+  overlayBG.classList.toggle("active");
+  body.classList.toggle("activeMenu");
+});
+
+const isValidEmail = (email) => {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; // sample@g.co
+  return re.test(String(email).toLowerCase());
+};
+
+emailInput.addEventListener("keyup", () => {
+  if (!isValidEmail(emailInput.value)) {
+    emailInput.classList.add("invalid");
+    invalidEmailLabel.style.setProperty("display", "block");
+  } else {
+    invalidEmailLabel.style.setProperty("display", "none");
+    emailInput.classList.remove("invalid");
+  }
+});
+
+window.addEventListener("load", (event) => {
+  invalidEmailLabel.value = "mm";
 });
 
 var swiper = new Swiper(".testimonial__list", {
